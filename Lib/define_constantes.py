74f5e3b0da4d123236a84ebf4336fcd502061952
jmdevilleros <jmdevilleros@gmail.com>
2024-08-06 23:58:58 -0500

    Commit inicial

---
+++ Lib/define_constantes.py
@@ -0,0 +1,7 @@
+# Definición de constantes para el proyecto
+
+DIRECTORIO_PROYECTO = r'D:\Desarrollo\JupyterNotebooks\Modelo_deterioro_tramos_v02'
+DIRECTORIO_DATOS    = f'{DIRECTORIO_PROYECTO}/Datos'
+DIRECTORIO_MODELOS  = f'{DIRECTORIO_PROYECTO}/Modelos'
+DIRECTORIO_LIB      = f'{DIRECTORIO_PROYECTO}/Lib'
+DIRECTORIO_GRAFICAS = f'{DIRECTORIO_PROYECTO}/Gráficas'
